46828d1ce602a29b8187185c8fb78334c5fc20eb
Kesara411 <60663650+Kesara411@users.noreply.github.com>
2020-05-05 20:22:40 -0400

    Add files via upload

---
+++ dev/Indrajith_report/Combined.sql
@@ -0,0 +1,19 @@
+SELECT 	M.year AS Election_Year, 
+		M.state_po AS State, 
+		M.candidate AS Candidate_Name, 
+		M.party AS Party_Affiliation, 
+		M.candidatevotes AS Votes
+		-- M.totalvotes
+
+FROM 	MITResults M
+
+WHERE 	M.year <> 2012 
+		AND M.year <> 2016
+		AND M.party IS NOT NULL
+		AND M.candidate IS NOT NULL
+
+UNION
+
+SELECT	*
+
+FROM	votes2
